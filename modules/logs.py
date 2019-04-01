9ba7594ae7a79fa436ebf950afc7bedfea3a1164
JonnyBoy2000 <jonathon_lokken@yahoo.com>
2019-03-31 20:38:58 -0500

    Nobody else needs that

--- modules/logs.py
+++ modules/logs.py
@@ -1,7 +1,6 @@
 import discord
 from discord.ext import commands
 import datetime
-from utils import checks
 
 class GuildLogs(commands.Cog):
     def __init__(self, bot):
@@ -9,18 +8,14 @@ class GuildLogs(commands.Cog):
 
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
-        e = discord.Embed(colour=0x36393E)
-        e.description = f"<:check:534931851660230666> Joined guild {guild.name} with {guild.member_count} users. Now in {len(self.bot.guilds)} guilds."
-        e.set_footer(text=f"Joined guild on {datetime.datetime.utcnow().strftime('%A %d %B %Y at %H:%M:%S')}")
-        await self.bot.get_channel(550121953462059008).send(embed=e)
+        msg = f"<:check:534931851660230666> Joined guild {guild.name} with {guild.member_count} users. Now in {len(self.bot.guilds)} guilds."
+        print(msg)
         self.bot.logger.info(f"[GUILD JOIN] Joined guild {guild.name} with {guild.member_count} users. Now in {len(self.bot.guilds)} guilds.")
 
     @commands.Cog.listener()
     async def on_guild_remove(self, guild):
-        e = discord.Embed(colour=0x36393E)
-        e.description = f"<:tickred:539495579793883176> Left guild {guild.name} with {guild.member_count} users. Now in {len(self.bot.guilds)} guilds."
-        e.set_footer(text=f"Left guild on {datetime.datetime.utcnow().strftime('%A %d %B %Y at %H:%M:%S')}")
-        await self.bot.get_channel(550121953462059008).send(embed=e)
+        msg = f"<:tickred:539495579793883176> Left guild {guild.name} with {guild.member_count} users. Now in {len(self.bot.guilds)} guilds."
+        print(msg)
         self.bot.logger.info(f"[GUILD LEAVE]  Left guild {guild.name} with {guild.member_count} users. Now in {len(self.bot.guilds)} guilds.")
         
 def setup(bot):
